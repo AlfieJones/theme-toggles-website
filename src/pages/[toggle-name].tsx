2acc4fa8774a77ca495872e5d8de9e60e2652acc
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-03-07 18:08:52 +0000

    Update [toggle-name].tsx

--- src/pages/[toggle-name].tsx
+++ src/pages/[toggle-name].tsx
@@ -1,23 +1,22 @@
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter"
 import clsx from "clsx"
 // @ts-ignore
-import { vscDarkPlus } from "./../../node_modules/react-syntax-highlighter/dist/esm/styles/prism"
+import {
+  coldarkDark,
+  coldarkCold,
+} from "./../../node_modules/react-syntax-highlighter/dist/esm/styles/prism"
 // @ts-ignore
 import jsx from "./../../node_modules/react-syntax-highlighter/dist/esm/languages/prism/jsx"
 import { generateCode, toggles } from "../toggles/utilities"
 import React, { Fragment, useEffect, useState } from "react"
 import ReactHtmlParser, { convertNodeToElement } from "react-html-parser"
-import {
-  CheckCircleIcon,
-  CheckIcon,
-  SelectorIcon,
-  XIcon,
-} from "@heroicons/react/solid"
+import { CheckCircleIcon, XIcon } from "@heroicons/react/solid"
 import { ClipboardCopyIcon } from "@heroicons/react/solid"
 import { Listbox, Switch, Transition } from "@headlessui/react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import { toggles as togglesMeta } from "../toggles/data/meta"
 import Layout from "../layouts/main"
+import { useTheme } from "next-use-theme"
 
 const tabs = ["HTML", "JSX"]
 
@@ -79,6 +78,8 @@ export default function Toggles({ code, toggle }: any) {
 
   const [show, setShow] = useState(false)
 
+  const { theme } = useTheme()
+
   useEffect(() => {
     let container
     switch (selected) {
@@ -99,23 +100,23 @@ export default function Toggles({ code, toggle }: any) {
 
   return (
     <>
-      <h1 className="ml-4 text-6xl font-bold text-gray-700 dark:text-white">
+      <h1 className="ml-4 text-6xl font-bold text-zinc-700 dark:text-white">
         {toggle.name}
       </h1>
-      <p className="mt-4 ml-4 text-2xl text-gray-500 dark:text-gray-400">
+      <p className="mt-4 ml-4 text-2xl text-zinc-500 dark:text-zinc-400">
         {toggle.description}
       </p>
       <div className="flex flex-col items-center mt-12 md:items-start md:mt-24 md:flex-row">
         <div className="h-full px-6 mb-6 lg:px-12 md:mb-0 ">
-          <div className="p-6 first-line:rounded-md">
+          <div className="p-6 first-line:rounded-md ">
             {ReactHtmlParser(code.checkbox.html, {
               preprocessNodes: preprocessNodes(toggle.classesToggle),
               transform: transformFn,
             })}
           </div>
         </div>
-        <div className="w-full p-2 overflow-x-hidden rounded-md bg-dark-800">
-          <div className="flex flex-wrap-reverse mx-2 overflow-auto border-b border-dark-50">
+        <div className="w-full p-2 overflow-x-hidden rounded-md bg-zinc-50 dark:bg-dark-800">
+          <div className="flex flex-wrap-reverse mx-2 overflow-auto border-b dark:border-dark-50 border-zinc-300">
             <nav className="flex pr-16 space-x-2" aria-label="Tabs">
               {variants.map((tab) => (
                 <button
@@ -124,8 +125,8 @@ export default function Toggles({ code, toggle }: any) {
                   onClick={() => setSelected(tab)}
                   className={clsx(
                     selected === tab
-                      ? "border-blue-500 text-blue-500"
-                      : "border-transparent text-gray-400 hover:text-gray-200 hover:border-gray-300",
+                      ? "dark:border-blue-500 dark:text-blue-500 border-blue-600 text-blue-600"
+                      : "border-transparent text-zinc-400 dark:hover:text-zinc-200 hover:text-zinc-600 hover:border-zinc-300",
                     "whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm"
                   )}
                 >
@@ -137,24 +138,24 @@ export default function Toggles({ code, toggle }: any) {
               <Switch.Group as="div" className="flex items-center">
                 <Switch.Label
                   as="span"
-                  className="mr-3 text-sm text-gray-200 pointer-events-none"
+                  className="mr-3 text-sm pointer-events-none dark:text-zinc-200 text-zinc-700"
                 >
                   Reverse
                 </Switch.Label>
                 <Switch
                   checked={reversed}
                   onChange={setReversed}
-                  className="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none focus:ring-2 ring-offset-dark-800 focus:ring-offset-2 focus:ring-blue-500"
+                  className="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none focus:ring-2 ring-offset-zinc-50 dark:ring-offset-dark-800 focus:ring-offset-2 focus:ring-blue-500"
                 >
-                  <span className="sr-only">Use setting</span>
+                  <span className="sr-only">Use reversed</span>
                   <span
                     aria-hidden="true"
-                    className="absolute w-full h-full rounded-md pointer-events-none bg-dark-800"
+                    className="absolute w-full h-full rounded-md pointer-events-none dark:bg-dark-800 bg-zinc-50"
                   />
                   <span
                     aria-hidden="true"
                     className={clsx(
-                      reversed ? "bg-blue-600" : "bg-dark-50",
+                      reversed ? "bg-blue-600" : "dark:bg-dark-50 bg-zinc-200",
                       "pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200"
                     )}
                   />
@@ -162,7 +163,7 @@ export default function Toggles({ code, toggle }: any) {
                     aria-hidden="true"
                     className={clsx(
                       reversed ? "translate-x-5" : "translate-x-0",
-                      "pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200"
+                      "pointer-events-none absolute left-0 inline-block h-5 w-5 border border-zinc-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200"
                     )}
                   />
                 </Switch>
@@ -176,25 +177,28 @@ export default function Toggles({ code, toggle }: any) {
               >
                 <button type="button" className="ml-5 mr-2">
                   <ClipboardCopyIcon
-                    className="w-5 h-5 text-white"
+                    className="w-5 h-5 dark:text-white text-zinc-500"
                     aria-hidden="true"
                   />
                 </button>
               </CopyToClipboard>
             </div>
           </div>
-          <div className="overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-dark-50 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
-            <SyntaxHighlighter
-              language="jsx"
-              style={vscDarkPlus}
-              customStyle={{
-                padding: "1em 1em 2em",
-                margin: "none",
-                width: "fit-content",
-              }}
-            >
-              {activeCode}
-            </SyntaxHighlighter>
+          <div className="overflow-y-scroll scrollbar-thin dark:scrollbar-thumb-zinc-500 scrollbar-thumb-zinc-400 dark:scrollbar-track-dark-50 scrollbar-track-zinc-200 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+            {theme && (
+              <SyntaxHighlighter
+                language="jsx"
+                style={theme === "light" ? coldarkCold : coldarkDark}
+                customStyle={{
+                  padding: "1em 1em 2em",
+                  margin: "none",
+                  width: "fit-content",
+                  background: "inherit",
+                }}
+              >
+                {activeCode}
+              </SyntaxHighlighter>
+            )}
           </div>
         </div>
       </div>
@@ -224,13 +228,13 @@ export default function Toggles({ code, toggle }: any) {
                     />
                   </div>
                   <div className="ml-3 w-0 flex-1 pt-0.5">
-                    <p className="text-sm font-medium text-gray-900 dark:text-gray-50">
+                    <p className="text-sm font-medium text-zinc-900 dark:text-zinc-50">
                       Code copied!
                     </p>
                   </div>
                   <div className="flex flex-shrink-0 ml-4">
                     <button
-                      className="inline-flex text-gray-400 rounded-md dark:text-gray-200 hover:text-gray-500 focus:outline-none dark:ring-offset-dark-400 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                      className="inline-flex rounded-md text-zinc-400 dark:text-zinc-200 hover:text-zinc-500 focus:outline-none dark:ring-offset-dark-400 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                       onClick={() => {
                         setShow(false)
                       }}
