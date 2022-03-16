f551c2f89d75b221cadb763cfd35ab6326aed032
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-03-16 16:00:02 +0000

    Updating docs

--- src/components/code/code.tsx
+++ src/components/code/code.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from "react"
 import { CodeProps } from "./code.props";
 import { highlight, languages } from "prismjs";
+import pkg from "theme-toggles/package.json"
 import clsx from "clsx";
 
 export const CodeFormatter: FC<CodeProps> = ({
@@ -10,9 +11,10 @@ export const CodeFormatter: FC<CodeProps> = ({
 }: CodeProps) => {
       const name = className?.match(/(language-)([^\s]+)/) || [""];
       const lang = name[0].replace("language-", "");
+      const code = ((children || "") as string).replace("{html-version}", pkg.version)
   return (
     <pre className={clsx(className, "overflow-y-scroll scrollbar-thin dark:scrollbar-thumb-zinc-500 scrollbar-thumb-zinc-400 dark:scrollbar-track-dark-50 scrollbar-track-zinc-200 scrollbar-thumb-rounded-full scrollbar-track-rounded-full")} {...rest}>
-      <code className={className} dangerouslySetInnerHTML={{__html: highlight(children as string, languages[lang], lang)}}></code>
+      <code className={className} dangerouslySetInnerHTML={{__html: highlight(code, languages[lang], lang)}}></code>
     </pre>
   )
 }
