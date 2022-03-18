66e1cc6836bc49d0f4616df770ad0317d5b48de9
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-03-18 15:05:40 +0000

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,16 @@
 export default {
-  canonical: "https://toggles.dev",
-  titleTemplate: 'Theme Toggles | %s',
-  defaultTitle: 'Theme Toggles',
+  titleTemplate: "Theme Toggles | %s",
+  defaultTitle: "Theme Toggles",
   openGraph: {
-    type: 'website',
-    url: 'https://toggles.dev',
-    site_name: 'Theme Toggles',
+    type: "website",
+    site_name: "Theme Toggles",
     images: [
       {
         url: "https://toggles.dev/theme-toggles.png",
         width: 1920,
         height: 480,
-        alt: 'Theme Toggle Banner',
+        alt: "Theme Toggle Banner",
       },
     ],
   },
-};  
+};
